7ca287f4a00778b331fed8356f07aadc13a1f96c
almost.wang <almost.wang@cloudwise.com>
2023-08-09 10:39:11 +0800

    使用 Cropper.js 完成图片裁剪功能

--- src/components/ImageProcesser.vue
+++ src/components/ImageProcesser.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="image-processer">
+    <a-modal
+      title="裁剪图片"
+      v-model:open="showModal"
+      @ok="handleOk"
+      @cancel="showModal = false"
+      okText="确认"
+      cancelText="取消"
+    >
+      <div class="image-cropper">
+        <img :src="baseImageUrl" ref="cropperImage" id="processed-image" />
+      </div>
+    </a-modal>
     <div class="image-preview" :style="{ backgroundImage: backgrondUrl }"></div>
     <div class="image-process">
       <styled-uploader @success="handleFileUploaded"></styled-uploader>
+      <a-button @click="showModal = true">
+        <template v-slot:icon><ScissorOutlined /></template>
+        裁剪图片
+      </a-button>
+      <a-button @click="handleDelete">
+        <template v-slot:icon>
+          <DeleteOutlined />
+        </template>
+        删除图片
+      </a-button>
     </div>
   </div>
 </template>
@@ -10,16 +32,90 @@
 <script setup lang="ts">
 import { UploadResp } from "@/extraType";
 import StyledUploader from "./StyledUploader.vue";
-import { computed } from "vue";
+import { computed, ref, watch, nextTick } from "vue";
+import { message } from "ant-design-vue";
+import { ScissorOutlined, DeleteOutlined } from "@ant-design/icons-vue";
+import Cropper from "cropperjs";
+import axios from "axios";
+
+interface CropperData {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+const cropperImage = ref<HTMLImageElement>();
+
+const showModal = ref(false);
+let cropper: Cropper;
+let cropperData: CropperData | null;
+watch(showModal, async (newValue) => {
+  if (newValue) {
+    await nextTick();
+    if (cropperImage.value) {
+      cropper = new Cropper(cropperImage.value, {
+        checkOrientation: true,
+        crop(event) {
+          const { x, y, width, height } = event.detail;
+          cropperData = {
+            x: Math.floor(x),
+            y: Math.floor(y),
+            width: Math.floor(width),
+            height: Math.floor(height),
+          };
+        },
+      });
+    }
+  } else {
+    if (cropper) {
+      cropper.destroy();
+    }
+  }
+});
 
 const props = defineProps<{ value: string }>();
 
 const backgrondUrl = computed(() => `url(${props.value})`);
+const baseImageUrl = computed(() => props.value.split("?")[0]);
 
 const emit = defineEmits(["change"]);
 
 const handleFileUploaded = ({ resp }: { resp: UploadResp }) => {
+  message.success("上传成功");
   emit("change", resp.data[0]);
+};
+
+const handleDelete = () => {
+  emit("change", "");
+};
+
+const handleOk = () => {
+  if (cropperData) {
+    const { x, y, width, height } = cropperData;
+    let cropperURL = baseImageUrl.value;
+    cropperURL += `?x-oss-process=image/crop,x_${x},y_${y},w_${width},h_${height}`;
+    emit("change", cropperURL);
+    // 不使用阿里云，拿到截图再次上传
+    // cropper.getCroppedCanvas().toBlob((blob) => {
+    //   if (blob) {
+    //     const formData = new FormData();
+    //     formData.append("croppedImage", blob, "test.png");
+    //     axios
+    //       .post("http://localhost:7001/api/utils/upload-img", formData, {
+    //         headers: {
+    //           "Content-Type": "multipart/form-data",
+    //         },
+    //       })
+    //       .then((resp) => {
+    //         if (resp.data.errno === 0) {
+    //           emit("change", resp.data.data[0]);
+    //         }
+    //       });
+    //   }
+    // });
+  }
+  showModal.value = false;
 };
 </script>
 
@@ -33,5 +129,9 @@ const handleFileUploaded = ({ resp }: { resp: UploadResp }) => {
   height: 84px;
   border: 1px dashed #e6ebed;
   background: no-repeat 50% / contain;
+}
+#processed-image {
+  display: block;
+  max-width: 100%;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import Antd from "ant-design-vue";
-import LegoComponents from "lego-components";
-import "lego-components/dist/index.css";
+import LegoComponents from "@almost-cli/lego-components";
+import "@almost-cli/lego-components/dist/index.css";
+import "cropperjs/dist/cropper.css";
 
 const app = createApp(App);
 app.use(Antd).use(store).use(router).use(LegoComponents).mount("#app");
